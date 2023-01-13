d0b3f7b57ec4a4eca2f2bfbb05a599232df81ccf
The-Rul <raaulgs@gmail.com>
2023-01-13 17:46:34 +0100

    Commit a GitHub desde Eclipse

--- src/hola.java
+++ src/hola.java
@@ -4,6 +4,10 @@ public static void main(String[] args) {
 	//Alumno Raúl Gómez Sanz
 	System.out.println("Hola mundo!! Soy Raúl");
 	System.out.println("Cambios con el ultimo commit");
+	System.out.println("Ver los cambios desde ultimo commit 2 try");
 	System.out.println("adios");
+	System.out.println("Esto tambien esta cambiado");
+	
+	System.out.println("Ver otros cambio");
 }
 }
